8d45d982fb665a629809e0cd94b8eeda8ad34c15
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2021-03-30 08:59:49 +0800

    react route added

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: path.join(__dirname, "/dist"),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -35,6 +36,9 @@ module.exports = {
               },
             
         ]
+    },
+    devServer: {
+        historyApiFallback: true
     },
     plugins: [
         new MiniCssExtractPlugin({
